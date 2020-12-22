df379c0e8148739f009fa65dc152edb4db32f5a2
Munzir Suliman <munzir.suliman@rethinktechs.com>
2020-12-22 15:45:53 +0400

    alter the message

--- api/lib/mail.js
+++ api/lib/mail.js
@@ -7,8 +7,8 @@ function mail(email, fullname, code){
 	  to: email,
 	  from: 'noreply@dubaioutletmall.com',
 	  subject: 'Verification',
-	  text: `Dear ${fullname}, Congratulations! Santa has left a surprise gift for you. show this message at the customer service desk to receive your gift!. Wishing you a happy shopping!`,
-	  html: `<p>Dear ${fullname},</p>><p>Congratulations!<br/> Santa has left a surprise gift for you.</p><p> show this message at the customer service desk to receive your gift!.</p><br/><p>Wishing you a happy shopping!<br/>Dubai Outlet Mall Team</p>`,
+	  text: `Dear ${fullname}, Congratulations! Santa has left a surprise gift for you. show this message at the customer service desk to receive your gift!.Registration Code: ${code}. Wishing you a happy shopping!`,
+	  html: `<p>Dear ${fullname},</p><p>Congratulations! Santa has left a surprise gift for you.</p><br/><p>Show this message at the customer service desk to receive your gift!.</p><br/><p>Registration Code: <strong>${code}</strong></p><br/><p>Wishing you a happy shopping!<br/>Dubai Outlet Mall Team</p>`,
 	};
 
 	return sgMail.send(msg);
